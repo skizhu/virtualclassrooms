f76ef4f5567ebcd313de84debaadeb873b619a02
Skizhu <zachary.gopinath@ucc.on.ca>
2021-03-29 13:04:18 -0400

    Used Google's function rather than my own

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 //firebase.auth().signInWithPopup(provider);
 function test() {
-  console.log('hi')
   var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 }
 console.log('in file');
